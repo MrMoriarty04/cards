ee739325c15890b9b0e279f783fd71b5141295de
MrMoriarty04 <ramialjaber2004@gmail.com>
2025-07-14 02:21:35 +0300

    finish app

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,39 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Card } from "./card/card";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Card],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('cards');
+  posts=[
+    {
+      title:"Neet Tree",
+      imageUrl:"/tree.jpeg",
+      userName:"nature",
+      content:"I saw this awesome tree during my hike today."
+    },
+    {
+      title:"Snowy Mountain",
+      imageUrl:"/mountain.jpeg",
+      userName:"mountainlover",
+      content:"Here is a picture of a very wonderfull snowy mountain."
+    },
+    {
+      title:"Mountain Biking",
+      imageUrl:"/biking.jpeg",
+      userName:"biking1222",
+      content:"I did some biking today."
+    },
+    {
+      title:"Mountain Biking",
+      imageUrl:"/biking.jpeg",
+      userName:"biking1222",
+      content:"I did some biking today."
+    }
+  ];
+
 }
